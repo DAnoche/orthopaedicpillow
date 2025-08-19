d2debcd867a5ac1800d89b57df30440eff31d032
unknown <diether.noche@gmail.com>
2025-08-19 17:15:41 +0800

    Finalize the project

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,22 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Modal
+  $("#preModal").modal("show");
+
+  // Date
+  var now = new Date();
+  $(".curyear").text(now.getFullYear());
+
+  //   Product Image Selection
+  $(".product-image-selector .product").on("click", function () {
+    let newSrc = $(this).attr("src");
+
+    $(".main-product").attr("src", newSrc);
+    $(".product-image-selector .product").removeClass("active");
+
+    $(this).addClass("active");
+  });
+});
+
+function closeModal() {
+  $("#preModal").modal("hide");
+}
