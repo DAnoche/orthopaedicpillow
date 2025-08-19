f2001b5cc9f90b0c5156e8c90387fc0c65d1ce68
unknown <diether.noche@gmail.com>
2025-08-19 17:35:24 +0800

    Add checkout page mock simulation

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,55 @@ $(document).ready(function () {
   $("#preModal").modal("show");
 
   // Date
-  var now = new Date();
-  $(".curyear").text(now.getFullYear());
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const now = new Date();
+  const todayDay = now.getDate();
+  const currentMonthIndex = now.getMonth();
+  const currentYear = now.getFullYear();
+
+  const randomDate = function () {
+    let monthOffset = Math.floor(Math.random() * 4);
+    let tempDate = new Date(now);
+    tempDate.setMonth(tempDate.getMonth() - monthOffset);
+
+    const maxDay = Math.min(
+      todayDay,
+      new Date(tempDate.getFullYear(), tempDate.getMonth() + 1, 0).getDate()
+    );
+    const randomDay = Math.floor(Math.random() * maxDay) + 1;
+
+    tempDate.setDate(randomDay);
+
+    const formattedMonth = monthNames[tempDate.getMonth()];
+    const formattedDay = tempDate.getDate();
+    const formattedYear = tempDate.getFullYear();
+
+    return `${formattedMonth} ${formattedDay}, <span class="nowYear">${formattedYear}</span>`;
+  };
+
+  $(".curMonth").text(monthNames[currentMonthIndex]);
+  $(".prevMonth").text(
+    monthNames[currentMonthIndex === 0 ? 11 : currentMonthIndex - 1]
+  );
+  $(".curYear").text(currentYear);
+
+  $(".date").each(function () {
+    $(this).html(randomDate());
+  });
 
   //   Product Image Selection
   $(".product-image-selector .product").on("click", function () {
@@ -19,4 +66,15 @@ $(document).ready(function () {
 
 function closeModal() {
   $("#preModal").modal("hide");
+}
+
+function checkout() {
+  let $toast = $("#toast");
+
+  $toast.addClass("show");
+
+  setTimeout(() => {
+    $toast.removeClass("show");
+    window.location.href = "checkout.html";
+  }, 1500);
 }
